be6eaef9716f0f54e613e7c19fd002d1567a1efc
absszerro <cryinghostt.56@gmail.com>
2023-06-05 11:05:08 +0300

    last refactoring

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function getData(page) {
   return fetch(url).then((response) => response.json());
 }
 
-// Получение данных из публичного API: name, email, text и тд
+// Получение данных из публичного API: id, img и тд
 function getPrizeImg(id) {
   const url = `https://jsonplaceholder.typicode.com/photos/${id}`;
   return fetch(url).then((response) => response.json());
@@ -39,32 +39,38 @@ function createTableRow(name, email, img, prize) {
   emailCell.textContent = email;
   row.appendChild(emailCell);
 
-  const prizeImage = document.createElement("img");
-  prizeImage.setAttribute("alt", "Ваш приз");
-  prizeImage.setAttribute("src", img); // Устанавливаем URL изображения
-  prizeImage.style.height = "100px";
-  prizeImage.style.width = "auto"; // Устанавливаем ширину изображения
-  row.appendChild(prizeImage);
-
   const prizeCell = document.createElement("td");
   prizeCell.setAttribute("data-label", "Приз");
-  prizeCell.textContent = prize;
+
+  const prizeImage = document.createElement("img");
+  prizeImage.setAttribute("src", img);
+  prizeImage.setAttribute("alt", "Ваш приз");
+  prizeImage.style.height = "100px";
+  prizeImage.style.width = "auto";
+  prizeCell.appendChild(prizeImage);
+
+  const prizeText = document.createElement("span");
+  prizeText.textContent = prize;
+  prizeCell.appendChild(prizeText);
+
   row.appendChild(prizeCell);
 
   return row;
 }
 
+// функция, скрывающая ссылку "Показать еще"
 function hideShowMoreLink() {
   const showMoreLink = document.querySelector("a#show-more-link");
   if (showMoreLink) {
-    showMoreLink.classList.add("hidden");
+    showMoreLink.style.display = "none";
   }
 }
 
-function unhideShowMoreLink() {
+// функция, возвращающая ссылку "Показать еще"
+function displayShowMoreLink() {
   const showMoreLink = document.querySelector("a#show-more-link");
   if (showMoreLink) {
-    showMoreLink.classList.remove("hidden");
+    showMoreLink.style.display = "";
   }
 }
 
@@ -77,45 +83,54 @@ function fillTableWithData(data) {
   tableBody.innerHTML = ""; // Очистка содержимого таблицы
 
   data.forEach((item) => {
-    const { id, name, email, body } = item; // Заменяем userId на postId, чтобы получить уникальное значение для изображения
-
+    const { id, name, email, body } = item;
+    // засекречивание половины email'а полльзователя
     const atIndex = email.indexOf("@");
 
     if (atIndex !== -1) {
       const username = email.substring(0, atIndex);
       const halfLength = Math.trunc(username.length / 2);
-      const hiddenPart = username.substring(0, halfLength) + "*".repeat(username.length - halfLength);
+      const hiddenPart =
+        username.substring(0, halfLength) +
+        "*".repeat(username.length - halfLength);
       const hiddenEmail = hiddenPart + email.substring(atIndex);
 
-      getPrizeImg(id) // Заменяем userId на postId
+      getPrizeImg(id)
         .then((imgData) => {
-          const row = createTableRow(name, hiddenEmail, imgData.thumbnailUrl, body.slice(Math.trunc(body.length / 2)));
-          tableBody.appendChild(row);
-          unhideShowMoreLink();
+          const row = createTableRow(
+            name,
+            hiddenEmail,
+            imgData.thumbnailUrl,
+            body.slice(Math.trunc(body.length / 2))
+          );
+          tableBody.appendChild(row); // добавление заполненной строки в таблицу
+          displayShowMoreLink();
         })
         .catch((error) => console.log(error));
     }
   });
 
-  tableBody.classList.add("slide-in-from-right");
-  
+  tableBody.classList.add("slide-in-from-right"); // активируем анимацию по клику на табы
+
   // Удаление класса анимации через небольшой интервал времени (чтобы анимация сработала повторно при следующем клике)
   setTimeout(() => {
     tableBody.classList.remove("slide-in-from-right");
   }, 600);
 }
 
-
+// вывод данных в таблицу (пагинация)
 function showWinners(week) {
   getData(week)
     .then(fillTableWithData)
     .catch((error) => console.log(error));
 }
 
+// установка таба по умолчанию (сразу открывается список из 1-x 10ти победителей - page 1)
 var activeButton = document.getElementById("week1");
 activeButton.classList.add("active");
 showWinners(1);
 
+// обработчик событий нажатия на таб
 function handleWeekButtonClick(week) {
   if (activeButton) {
     activeButton.classList.remove("active");
